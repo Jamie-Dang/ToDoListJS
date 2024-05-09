48731e7fd8dbb0f05fa35be22bac700da0f21e85
Tuyen Dang <tuyendang878@gmail.com>
2024-05-09 10:57:50 +0700

    update

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-// Thêm biểu tưởng "checked" khi item được "click"
+// Thêm biểu tưởng "checked" khi item được "click": Đánh dấu hoàn thành task
 var list = document.querySelector("ul");
 list.addEventListener(
   "click",
@@ -11,30 +11,22 @@ list.addEventListener(
 );
 
 // Tạo item mới khi nhấn nút "Add"
+// Định nghĩa hàm có tên: newElement()
 function newElement() {
   var li = document.createElement("li");
+  // Lấy giá trị nhập từ bàn phím (người dùng)
   var inputValue = document.getElementById("myInput").value;
+  // Tạo một đối tượng TextNode mới chứa giá trị văn bản được nhập từ ô input
   var t = document.createTextNode(inputValue);
+  // Thêm đối tượng TextNode vào phần tử <li>
   li.appendChild(t);
-  if (inputValue === "") {
-    alert("You must write something!");
-  } else {
-    document.getElementById("myUL").appendChild(li);
-  }
+  // Kiểm tra nếu người dùng không nhập gì vào ô input, hiển thị một cảnh báo yêu cầu nhập dữ liệu. Nếu có dữ liệu được nhập, thêm mục mới vào danh sách
+  // if (inputValue === "") {
+  //   alert("You must write something!");
+  // } else {
+  document.getElementById("myUL").appendChild(li);
+  // }
   document.getElementById("myInput").value = "";
-
-  var span = document.createElement("span");
-  var txt = document.createTextNode("\u00D7");
-  span.className = "close";
-  span.appendChild(txt);
-  li.appendChild(span);
-
-  for (i = 0; i < close.length; i++) {
-    close[i].onclick = function () {
-      var div = this.parentElement;
-      div.style.display = "none";
-    };
-  }
 }
 
 // Tạo nút "đóng" và thêm nó vào mỗi item
